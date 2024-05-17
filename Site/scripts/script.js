14c8fd5dbb7f8bdc77f1c20792915896707b4570
JaoPedr0o <joaopedromp15@outlook.com>
2024-05-17 20:06:51 -0300

    UPDATE: Reformulando Design do site 03

--- Site/scripts/script.js
+++ Site/scripts/script.js
@@ -4,22 +4,6 @@ let btOpenList = document.getElementById("open-list")
 let btCloseList = document.getElementById("close-list")
 let headerBanner =  document.getElementById("header-banner")
 
-//Função que faz a rotação do banner da home 
-//NÃO ESTÁ FUNCIONANDO
-// headerBanner.style.backgroundImage = "url(/assets/rotative-banner/fruits1-img.jpg)"
-function rotativeBanner() {
-  let bannerTime = 0
-  setInterval(() => {
-    if (bannerTime < 4) {
-      bannerTime = bannerTime + 1
-    } else if (bannerTime = 4 ){
-      bannerTime = 1
-    }
-    headerBanner.style.backgroundImage = "url(/assets/rotative-banner/fruits"+bannerTime+"-img.jpg)"
-  }, 10000);
-  rotativeBanner()
-}
-// rotativeBanner()
 
 
 //Abertura e fechamento da modal
