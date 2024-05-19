8e7bdabfd81d71b3f0cccb49cd017922662de7aa
João Pedro <joaopedromp15@outlook.com>
2024-05-19 18:28:50 -0300

    UPDATE: Reformulando Design do site 05

--- Site/scripts/generator.js
+++ Site/scripts/generator.js
@@ -3,64 +3,102 @@ const productList = [
 // Adiciona produtos dinamicamente
 ];
 
-//Função que processa a adição de produtos à lista
-function adicionarCompra(productName, productPrice, productCountUnit, productCountKg) {
-    let cardProduto = document.querySelector(".card-produto")
-    let qtd_unit = parseInt(productCountUnit)
-    let qtd_kg = parseFloat(productCountKg)
+//Lista de Preços de cada produto
+const productPriceList = [
+    {
+    "name": "Mamão",
+    "price": 3.5,
+    },
+    {
+    "name": "Banana Prata",
+    "price": 4.5,
+    },
+    {
+    "name": "Pitahaya",
+    "price": 5.00,
+    },
+    {
+    "name": "Coco",
+    "price": 6.00,
+    },
+    {
+    "name": "Figo",
+    "price": 2.00,
+    },
+    {
+    "name": "Pera",
+    "price": 1.75,
+    },
+    {
+    "name": "Morango",
+    "price": 8.00,
+    },
+    {
+    "name": "Limão Siciliano",
+    "price": 1.50,
+    },
+    {
+    "name": "Manga Espada",
+    "price": 4.80,
+    },
+    {
+    "name": "Abacate",
+    "price": 3.90,
+    },
+]
 
+const lista = document.getElementById('lista');
+
+//Função que processa a adição de produtos à lista | Ela traz o conteudo do imput no html
+function adicionarCompra(product) {
+    //Busca o preço do produto escolhido dentro do array de objetos productListprice.  
+    let productData = productPriceList.find(produto => produto.name == product.name);
+    let productPrice = parseFloat(productData.price*product.value)
+    let productName = (product.name)
+    let productCount = parseInt(product.value)
+    
     //Verifica se o produto existe na lista
     let produtoExist = productList.find(item => item.nome === productName);
     
-    if (produtoExist) {
-        // Se o produto já estiver na lista, aumenta a quantidade
-        if (productCountKg !== 0 && productCountUnit > 0) { //Se for em unidade
-            produtoExist.quantidade += qtd_unit;
-            produtoExist.preco += productPrice*qtd_unit;
-            atualizaLista(productName, qtd_unit, productCountUnit*productPrice)
-        } else if (productCountUnit !== 0 && productCountKg > 0) { //Se for em quilogramas
-            produtoExist.quantidade += qtd_kg;
-            produtoExist.preco += productPrice*qtd_kg;
-        }  
+    if (produtoExist === undefined) {
+        if (productCount !== 0) {
+            // Se não, adiciona o produto com a quantidade especificada
+            productList.push({
+                nome: productName,
+                preco: productPrice,
+                quantidade: productCount,
+            })
+            
+            //Aqui ele busca no html a tag com id="lista" e cria dentro um "li" indicando o item adicionado, quantidade e valor.
+            const li = document.createElement("li");
+            li.id = `${productName}`;
+            const btRemove = document.createElement('button')
+
+            li.textContent = `${productName} - ${productCount} Unidade(s) = R$${productPrice}`;
+
+            btRemove.textContent = "Remover"
+            btRemove.id = `remove-${productName}`
+
+            lista.appendChild(li);
+            li.appendChild(btRemove)
+
+            //Troca a imagem do carrinho de compras mostrando que algo foi adicionado
+            let cartIcon = document.getElementById("cart-icon")
+            cartIcon.style.backgroundImage = "url(/Site/assets/icons/lista-de-controle.png)"
+            console.log(productList)
+        }
     } else {
-        // Se não, adiciona o produto com a quantidade especificada
-        if (productCountKg !== 0 && productCountUnit > 0) { //Se for em unidade
-            productList.push({
-                nome: productName,
-                preco: productPrice*qtd_unit,
-                quantidade: qtd_unit,
-            })
-            atualizaLista(productName, qtd_unit, productCountUnit*productPrice)
-        } else if (productCountUnit !== 0 && productCountKg > 0) { //Se for em quilogramas
-            productList.push({
-                nome: productName,
-                preco: productPrice*qtd_kg,
-                quantidade: qtd_kg,
-            })
-            atualizaLista(productName, qtd_kg, productCountKg*productPrice)
-        } 
-        //Troca a imagem do carrinho de compras mostrando que algo foi adicionado
-        let cartIcon = document.getElementById("cart-icon")
-        cartIcon.style.backgroundImage = "url(/Site/assets/icons/lista-de-controle.png)"
-        console.log(productList)
+        // Se o produto já estiver na lista, aumenta a quantidade
+        if (productCount !== 0) {
+            let li = document.getElementById(`${productName}`)
+
+            const btRemove = document.createElement('button')
+
+            li.textContent = `${productName} - ${produtoExist.quantidade += productCount} Unidade(s) = R$${produtoExist.preco += productPrice}`;
+
+            btRemove.textContent = "Remover"
+            btRemove.id = `remove-${productName}`
+            li.appendChild(btRemove)
+        }
     }
-}
-
-function atualizaLista(productName, productCount ,productPrice) {
-    //Aqui ele busca no html a tag com id="lista" e cria dentro um "li" indicando o item adicionado, quantidade e valor.
-    const lista = document.getElementById('lista');
-    const li = document.createElement('li');
-    const btRemove = document.createElement('button')
-    const btAdd = document.createElement('button')
-
-    li.textContent = `${productName}: ${productCount}: R$${productPrice}`;
-
-    btRemove.textContent = "Remover"
-    btAdd.textContent = "Adicionar"
-    btRemove.classList.add("btRemove")
-    btAdd.classList.add("btAdd")
-
-    lista.appendChild(li);
-    li.appendChild(btRemove)
-    li.appendChild(btAdd)
 }
--- Site/scripts/script.js
+++ Site/scripts/script.js
@@ -3,14 +3,22 @@ let modal = document.getElementById("modal-list")
 let btOpenList = document.getElementById("open-list")
 let btCloseList = document.getElementById("close-list")
 let headerBanner =  document.getElementById("header-banner")
+let body = document.querySelector("body")
+
+let cart = document.getElementById("cart-page")
 
 
 
 //Abertura e fechamento da modal
 btOpenList.addEventListener("click", function () {
   modal.classList.remove("modal-active")
+  body.style.overflow = "hidden"
+  body.style.marginRight = "0.9%"
 })
 
 btCloseList.addEventListener("click", function () {
   modal.classList.add("modal-active")
+  body.style.overflow = "visible"
+  body.style.overflowX = "hidden"
+  body.style.marginRight = "0"
 })
