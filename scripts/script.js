bc993356969b521b47b39fe3ea83dd2940182d27
JaoPedr0o <joaopedromp15@outlook.com>
2024-06-16 18:22:44 -0300

    UPDATE: Box de informação sobre preços e mais algumas alterações gerais

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,32 @@
 //Declaração de Variáveis
 let modal = document.getElementById("modal-list")
-let btOpenList = document.getElementById("open-list")
+let btOpenList = document.getElementsByClassName("open-list")
 let btCloseList = document.getElementById("close-list")
 let headerBanner =  document.getElementById("header-banner")
 let body = document.querySelector("body")
 let cart = document.getElementById("cart-page")
-
+let modalInfoBtn = document.getElementById("modal-info-btn")
+let modalInfo = document.getElementById("modal-info")
 
 
 //Abertura e fechamento da modal
-btOpenList.addEventListener("click", function () {
-  modal.classList.remove("modal-active")
-  body.style.overflow = "hidden"
-})
+btOpenList.forEach(element => {
+  element.addEventListener("click", function () {
+    modal.classList.remove("modal-active")
+    body.style.overflow = "hidden"
+  })
+});
+
 
 btCloseList.addEventListener("click", function () {
   modal.classList.add("modal-active")
   body.style.overflow = "visible"
   body.style.overflowX = "hidden"
 })
+
+
+modalInfoBtn.addEventListener("click", function () {
+  modalInfo.classList.toggle("modal-active")
+})
+
 
